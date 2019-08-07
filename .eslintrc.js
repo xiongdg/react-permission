f4ac65b169019fbd8d58f88e507b4fdaebd92220
leiwenpeng <15608071173@163.com>
2019-08-07 18:24:29 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,13 @@
  */
 module.exports = {
     extends: ['eslint:recommended', 'prettier'],
+    parser: 'babel-eslint',
     env: {
         browser: true,
         commonjs: true,
         node: true,
-        es6: true
+        es6: true,
+        jest: true
     },
     parserOptions: {
         ecmaVersion: 2018,
