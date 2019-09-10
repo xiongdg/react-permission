d5850651141b4ba7f472351c390b49d92a383090
leiwenpeng <15608071173@163.com>
2019-09-10 15:19:05 +0800

    添加useAuth文件

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,5 +2,13 @@
  * @author ray
  * @description 获取权限
  */
+import { useContext } from 'react';
+import ReactAuthContext from '../Context';
 
-export default function useAuth() {}
+export default function useAuth() {
+    const context = useContext(ReactAuthContext);
+
+    const { permissions } = context;
+
+    return [permissions];
+}
