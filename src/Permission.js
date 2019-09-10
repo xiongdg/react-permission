d15ff926d84176416907f80d40d8c9ee896ac4bd
leiwenpeng <15608071173@163.com>
2019-09-10 09:41:49 +0800

    update

---
+++ src/Permission.js
@@ -0,0 +1,16 @@
+/**
+ * @author Ray
+ * @description 以装饰器形式提供类组件接入权限控制的方法
+ */
+
+/**
+ * @description 权限装饰器
+ * @param key
+ * @returns {Function}
+ */
+// const permission = key => (target, props, descriptor) => {
+//     let raw = descriptor.value;
+//     return target;
+// };
+//
+// export default permission;
