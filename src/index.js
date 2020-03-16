a3680c23e32294d45f93335e127315e2b5ee1d73
leiwenpeng <leiwenpeng1994@outlook.com>
2020-03-16 20:35:04 +0800

    remove hooks api

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@
  * @description 权限的入口组件
  */
 export { default as PermissionProvider } from './provider';
-export { default as usePermissions } from './hook/useAuth';
 export { default as Permission } from './permission';
--- src/provider.js
+++ src/provider.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 export const AuthContext = React.createContext({});
 
 export const ProviderProps = {
-  children: PropTypes.arrayOf(PropTypes.element),
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element)
+  ]),
   permissions: PropTypes.arrayOf(PropTypes.number),
   fallback: PropTypes.element
 };
@@ -15,7 +18,7 @@ export const ProviderDefaultProps = {
   fallback: <Fragment />
 };
 
-Provider.propTypes = {};
+Provider.propTypes = ProviderProps;
 
 Provider.defaultProps = ProviderDefaultProps;
 
